1b919ed2c84fa95d4219ad3df3a912ce96f51c47
Никита Болтовский <145008308+Nikkitika@users.noreply.github.com>
2024-09-29 21:59:18 +0300

    Create math_operations.h

---
+++ math_operations.h
@@ -0,0 +1,16 @@
+#ifndef MATH_OPERATIONS_H
+#define MATH_OPERATIONS_H
+
+// Функция для сложения двух чисел
+int add(int a, int b);
+
+// Функция для вычитания второго числа из первого
+int subtract(int a, int b);
+
+// Функция для умножения двух чисел
+int multiply(int a, int b);
+
+// Функция для деления первого числа на второе
+int divide(int a, int b);
+
+#endif // MATH_OPERATIONS_H
